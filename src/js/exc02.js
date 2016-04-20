d23b95511120d8cfa8efe1deb6c9937b7b44e9df
Roger Sobrado <roger.sobrado@stkhp.com>
2016-04-20 11:50:36 -0600

    json update

--- src/js/exc02.js
+++ src/js/exc02.js
@@ -1,4 +1,5 @@
-var usersArray2 = users2;
+var usersArray2;
+
 //--------- funciones mantenimiento usuarios 
 //-----------------------------------------
 function addUser02(userName){
@@ -41,27 +42,30 @@ function deleteUser02(userIndex){
 function createUsersList02(){
 
 	var userList = '<ul class="demo-list-two mdl-list">'
+  var btnReset = '<button class="mdl-button mdl-js-button mdl-button--raised  mdl-button--colored" onclick="resetUsers()"> RESET </button>';
+  var btnLoad = '<button class="mdl-button mdl-js-button mdl-button--raised  mdl-button--accent" onclick="reloadUsersList02()"> LOAD </button>';
+  var btns = '<div class="buttons">'+btnLoad+btnReset+'';
 
   for (var i = 0; i <= users2.length - 1; i++) {
 
     var _mdl_com = '</span><span class="mdl-list__item-sub-title">' + getPostByUsers02(users2[i].id) +' posts</span></span>'; 
-    var btn = '<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--colored" onclick="deleteUser02('+i+')"> DELETE </button>'
-    var _mdl_cardContent01  = '<div class="mdl-card__actions mdl-card--border">Total users: <b>'+users2.length+'</b></div><div class="mdl-card__menu"></div></div></div>'
+    var btn = '<button class="mdl-button mdl-js-button mdl-button--raised  mdl-button--colored" onclick="deleteUser02('+i+')"> DELETE </button>';
+    var _mdl_cardContent01  = '<div class="mdl-card__actions mdl-card--border">Total users: <b>'+users2.length+'</b></div></div>'
     var _mdl_cardLi01  = '<li class="mdl-list__item mdl-list__item--two-line"><span class="mdl-list__item-primary-content"> <i class="material-icons mdl-list__item-avatar">person</i><span>';
 
 		userList += _mdl_cardLi01+users2[i].name + _mdl_com+btn+' </li>'
 	}
 
-	userList += '</ul></div>'+_mdl_cardContent01;
+	userList += '</ul></div>'+_mdl_cardContent01+btns;
   // console.log(userListTotal);
  	return userList;
 }
 
 function getPostByUsers02(id){
   var postCounter = 0;
-  for (var i = 0; i <= posts.length - 1; i++) {
+  for (var i = 0; i <= posts2.length - 1; i++) {
 
-    if(id == posts[i].userId){
+    if(id == posts2[i].userId){
       postCounter++;
     }
   }
@@ -78,7 +82,7 @@ function reloadUsersList02(){
 function loadEx02(){
   document.getElementById('app').innerHTML = '';
   document.getElementById('app').innerHTML =  mdl_header(2);
-  reloadUsersList02();
+  resetUsers()
 }
 
 function unLoadEx02(){
@@ -86,29 +90,46 @@ function unLoadEx02(){
 }
 
 
-var getJSON = function(url) {
-  return new Promise(function(resolve, reject) {
+function resetUsers(){
+  getJSON('https://jsonplaceholder.typicode.com/users', function(err, data) {
+    if (err != null) {
+      alert('Something went wrong: ' + err);
+    } else {
+      users2 = data;
+    }
+  });
+  reloadUsersList02();
+}
+
+var getJSON = function(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('get', url, true);
-    xhr.responseType = 'jsonp';
+    xhr.responseType = 'json';
     xhr.onload = function() {
       var status = xhr.status;
       if (status == 200) {
-        resolve(xhr.response);
+        callback(null, xhr.response);
       } else {
-        reject(status);
+        callback(status);
       }
     };
     xhr.send();
-  });
 };
 
-getJSON('https://jsonplaceholder.typicode.com/users').then(function(data) {
-    alert('Your Json result is:  ' + data.result); //you can comment this, i used it to debug
 
-    result.innerText = data.result; //display the result in an HTML element
-}, function(status) { //error detection....
-  alert('Something went wrong.');
-});
+getJSON('https://jsonplaceholder.typicode.com/users', function(err, data) {
+    if (err != null) {
+      alert('Something went wrong: ' + err);
+    } else {
+      users2 = data;
+    }
+  });
 
 
+getJSON('https://jsonplaceholder.typicode.com/posts', function(err, data) {
+    if (err != null) {
+      alert('Something went wrong: ' + err);
+    } else {
+      posts2 = data;
+    }
+  });
--- src/js/globals.js
+++ src/js/globals.js
@@ -255,171 +255,12 @@ var users = [
     }
   }
 ];
-
-var users2 = [
-  {
-    "id": 1,
-    "name": "Leanne Graham",
-    "username": "Bret",
-    "email": "Sincere@april.biz",
-    "address": {
-      "street": "Kulas Light",
-      "suite": "Apt. 556",
-      "city": "Gwenborough",
-      "zipcode": "92998-3874",
-      "geo": {
-        "lat": "-37.3159",
-        "lng": "81.1496"
-      }
-    },
-    "phone": "1-770-736-8031 x56442",
-    "website": "hildegard.org",
-    "company": {
-      "name": "Romaguera-Crona",
-      "catchPhrase": "Multi-layered client-server neural-net",
-      "bs": "harness real-time e-markets"
-    }
-  },
-  {
-    "id": 2,
-    "name": "Ervin Howell",
-    "username": "Antonette",
-    "email": "Shanna@melissa.tv",
-    "address": {
-      "street": "Victor Plains",
-      "suite": "Suite 879",
-      "city": "Wisokyburgh",
-      "zipcode": "90566-7771",
-      "geo": {
-        "lat": "-43.9509",
-        "lng": "-34.4618"
-      }
-    },
-    "phone": "010-692-6593 x09125",
-    "website": "anastasia.net",
-    "company": {
-      "name": "Deckow-Crist",
-      "catchPhrase": "Proactive didactic contingency",
-      "bs": "synergize scalable supply-chains"
-    }
-  },
-  {
-    "id": 3,
-    "name": "Clementine Bauch",
-    "username": "Samantha",
-    "email": "Nathan@yesenia.net",
-    "address": {
-      "street": "Douglas Extension",
-      "suite": "Suite 847",
-      "city": "McKenziehaven",
-      "zipcode": "59590-4157",
-      "geo": {
-        "lat": "-68.6102",
-        "lng": "-47.0653"
-      }
-    },
-    "phone": "1-463-123-4447",
-    "website": "ramiro.info",
-    "company": {
-      "name": "Romaguera-Jacobson",
-      "catchPhrase": "Face to face bifurcated interface",
-      "bs": "e-enable strategic applications"
-    }
-  },
-  {
-    "id": 4,
-    "name": "Patricia Lebsack",
-    "username": "Karianne",
-    "email": "Julianne.OConner@kory.org",
-    "address": {
-      "street": "Hoeger Mall",
-      "suite": "Apt. 692",
-      "city": "South Elvis",
-      "zipcode": "53919-4257",
-      "geo": {
-        "lat": "29.4572",
-        "lng": "-164.2990"
-      }
-    },
-    "phone": "493-170-9623 x156",
-    "website": "kale.biz",
-    "company": {
-      "name": "Robel-Corkery",
-      "catchPhrase": "Multi-tiered zero tolerance productivity",
-      "bs": "transition cutting-edge web services"
-    }
-  },
-  {
-    "id": 5,
-    "name": "Chelsey Dietrich",
-    "username": "Kamren",
-    "email": "Lucio_Hettinger@annie.ca",
-    "address": {
-      "street": "Skiles Walks",
-      "suite": "Suite 351",
-      "city": "Roscoeview",
-      "zipcode": "33263",
-      "geo": {
-        "lat": "-31.8129",
-        "lng": "62.5342"
-      }
-    },
-    "phone": "(254)954-1289",
-    "website": "demarco.info",
-    "company": {
-      "name": "Keebler LLC",
-      "catchPhrase": "User-centric fault-tolerant solution",
-      "bs": "revolutionize end-to-end systems"
-    }
-  },
-  {
-    "id": 6,
-    "name": "Mrs. Dennis Schulist",
-    "username": "Leopoldo_Corkery",
-    "email": "Karley_Dach@jasper.info",
-    "address": {
-      "street": "Norberto Crossing",
-      "suite": "Apt. 950",
-      "city": "South Christy",
-      "zipcode": "23505-1337",
-      "geo": {
-        "lat": "-71.4197",
-        "lng": "71.7478"
-      }
-    },
-    "phone": "1-477-935-8478 x6430",
-    "website": "ola.org",
-    "company": {
-      "name": "Considine-Lockman",
-      "catchPhrase": "Synchronised bottom-line interface",
-      "bs": "e-enable innovative applications"
-    }
-  }
-];
 var posts = [
   {
     "userId": 1,
     "id": 1,
     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-  },
-  {
-    "userId": 1,
-    "id": 2,
-    "title": "qui est esse",
-    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-  },
-  {
-    "userId": 1,
-    "id": 3,
-    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-  },
-  {
-    "userId": 1,
-    "id": 9,
-    "title": "nesciunt iure omnis dolorem tempora et accusantium",
-    "body": "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas"
   },
   {
     "userId": 1,
@@ -476,60 +317,6 @@ var posts = [
     "body": "eveniet quo quis\nlaborum totam consequatur non dolor\nut et est repudiandae\nest voluptatem vel debitis et magnam"
   },
   {
-    "userId": 2,
-    "id": 19,
-    "title": "adipisci placeat illum aut reiciendis qui",
-    "body": "illum quis cupiditate provident sit magnam\nea sed aut omnis\nveniam maiores ullam consequatur atque\nadipisci quo iste expedita sit quos voluptas"
-  },
-  {
-    "userId": 2,
-    "id": 20,
-    "title": "doloribus ad provident suscipit at",
-    "body": "qui consequuntur ducimus possimus quisquam amet similique\nsuscipit porro ipsam amet\neos veritatis officiis exercitationem vel fugit aut necessitatibus totam\nomnis rerum consequatur expedita quidem cumque explicabo"
-  },
-  {
-    "userId": 3,
-    "id": 21,
-    "title": "asperiores ea ipsam voluptatibus modi minima quia sint",
-    "body": "repellat aliquid praesentium dolorem quo\nsed totam minus non itaque\nnihil labore molestiae sunt dolor eveniet hic recusandae veniam\ntempora et tenetur expedita sunt"
-  },
-  {
-    "userId": 3,
-    "id": 22,
-    "title": "dolor sint quo a velit explicabo quia nam",
-    "body": "eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse"
-  },
-  {
-    "userId": 3,
-    "id": 23,
-    "title": "maxime id vitae nihil numquam",
-    "body": "veritatis unde neque eligendi\nquae quod architecto quo neque vitae\nest illo sit tempora doloremque fugit quod\net et vel beatae sequi ullam sed tenetur perspiciatis"
-  },
-  {
-    "userId": 3,
-    "id": 24,
-    "title": "autem hic labore sunt dolores incidunt",
-    "body": "enim et ex nulla\nomnis voluptas quia qui\nvoluptatem consequatur numquam aliquam sunt\ntotam recusandae id dignissimos aut sed asperiores deserunt"
-  },
-  {
-    "userId": 3,
-    "id": 25,
-    "title": "rem alias distinctio quo quis",
-    "body": "ullam consequatur ut\nomnis quis sit vel consequuntur\nipsa eligendi ipsum molestiae et omnis error nostrum\nmolestiae illo tempore quia et distinctio"
-  },
-  {
-    "userId": 3,
-    "id": 26,
-    "title": "est et quae odit qui non",
-    "body": "similique esse doloribus nihil accusamus\nomnis dolorem fuga consequuntur reprehenderit fugit recusandae temporibus\nperspiciatis cum ut laudantium\nomnis aut molestiae vel vero"
-  },
-  {
-    "userId": 3,
-    "id": 27,
-    "title": "quasi id et eos tenetur aut quo autem",
-    "body": "eum sed dolores ipsam sint possimus debitis occaecati\ndebitis qui qui et\nut placeat enim earum aut odit facilis\nconsequatur suscipit necessitatibus rerum sed inventore temporibus consequatur"
-  },
-  {
     "userId": 3,
     "id": 28,
     "title": "delectus ullam et corporis nulla voluptas sequi",
@@ -552,30 +339,6 @@ var posts = [
     "id": 31,
     "title": "ullam ut quidem id aut vel consequuntur",
     "body": "debitis eius sed quibusdam non quis consectetur vitae\nimpedit ut qui consequatur sed aut in\nquidem sit nostrum et maiores adipisci atque\nquaerat voluptatem adipisci repudiandae"
-  },
-  {
-    "userId": 4,
-    "id": 32,
-    "title": "doloremque illum aliquid sunt",
-    "body": "deserunt eos nobis asperiores et hic\nest debitis repellat molestiae optio\nnihil ratione ut eos beatae quibusdam distinctio maiores\nearum voluptates et aut adipisci ea maiores voluptas maxime"
-  },
-  {
-    "userId": 4,
-    "id": 33,
-    "title": "qui explicabo molestiae dolorem",
-    "body": "rerum ut et numquam laborum odit est sit\nid qui sint in\nquasi tenetur tempore aperiam et quaerat qui in\nrerum officiis sequi cumque quod"
-  },
-  {
-    "userId": 4,
-    "id": 34,
-    "title": "magnam ut rerum iure",
-    "body": "ea velit perferendis earum ut voluptatem voluptate itaque iusto\ntotam pariatur in\nnemo voluptatem voluptatem autem magni tempora minima in\nest distinctio qui assumenda accusamus dignissimos officia nesciunt nobis"
-  },
-  {
-    "userId": 4,
-    "id": 35,
-    "title": "id nihil consequatur molestias animi provident",
-    "body": "nisi error delectus possimus ut eligendi vitae\nplaceat eos harum cupiditate facilis reprehenderit voluptatem beatae\nmodi ducimus quo illum voluptas eligendi\net nobis quia fugit"
   },
   {
     "userId": 4,
@@ -627,24 +390,6 @@ var posts = [
   },
   {
     "userId": 5,
-    "id": 46,
-    "title": "aut quo modi neque nostrum ducimus",
-    "body": "voluptatem quisquam iste\nvoluptatibus natus officiis facilis dolorem\nquis quas ipsam\nvel et voluptatum in aliquid"
-  },
-  {
-    "userId": 5,
-    "id": 47,
-    "title": "quibusdam cumque rem aut deserunt",
-    "body": "voluptatem assumenda ut qui ut cupiditate aut impedit veniam\noccaecati nemo illum voluptatem laudantium\nmolestiae beatae rerum ea iure soluta nostrum\neligendi et voluptate"
-  },
-  {
-    "userId": 5,
-    "id": 48,
-    "title": "ut voluptatem illum ea doloribus itaque eos",
-    "body": "voluptates quo voluptatem facilis iure occaecati\nvel assumenda rerum officia et\nillum perspiciatis ab deleniti\nlaudantium repellat ad ut et autem reprehenderit"
-  },
-  {
-    "userId": 5,
     "id": 49,
     "title": "laborum non sunt aut ut assumenda perspiciatis voluptas",
     "body": "inventore ab sint\nnatus fugit id nulla sequi architecto nihil quaerat\neos tenetur in in eum veritatis non\nquibusdam officiis aspernatur cumque aut commodi aut"
@@ -690,42 +435,6 @@ var posts = [
     "id": 56,
     "title": "qui et at rerum necessitatibus",
     "body": "aut est omnis dolores\nneque rerum quod ea rerum velit pariatur beatae excepturi\net provident voluptas corrupti\ncorporis harum reprehenderit dolores eligendi"
-  },
-  {
-    "userId": 6,
-    "id": 57,
-    "title": "sed ab est est",
-    "body": "at pariatur consequuntur earum quidem\nquo est laudantium soluta voluptatem\nqui ullam et est\net cum voluptas voluptatum repellat est"
-  },
-  {
-    "userId": 6,
-    "id": 58,
-    "title": "voluptatum itaque dolores nisi et quasi",
-    "body": "veniam voluptatum quae adipisci id\net id quia eos ad et dolorem\naliquam quo nisi sunt eos impedit error\nad similique veniam"
-  },
-  {
-    "userId": 6,
-    "id": 59,
-    "title": "qui commodi dolor at maiores et quis id accusantium",
-    "body": "perspiciatis et quam ea autem temporibus non voluptatibus qui\nbeatae a earum officia nesciunt dolores suscipit voluptas et\nanimi doloribus cum rerum quas et magni\net hic ut ut commodi expedita sunt"
-  },
-  {
-    "userId": 6,
-    "id": 60,
-    "title": "consequatur placeat omnis quisquam quia reprehenderit fugit veritatis facere",
-    "body": "asperiores sunt ab assumenda cumque modi velit\nqui esse omnis\nvoluptate et fuga perferendis voluptas\nillo ratione amet aut et omnis"
-  },
-  {
-    "userId": 7,
-    "id": 61,
-    "title": "voluptatem doloribus consectetur est ut ducimus",
-    "body": "ab nemo optio odio\ndelectus tenetur corporis similique nobis repellendus rerum omnis facilis\nvero blanditiis debitis in nesciunt doloribus dicta dolores\nmagnam minus velit"
-  },
-  {
-    "userId": 7,
-    "id": 62,
-    "title": "beatae enim quia vel",
-    "body": "enim aspernatur illo distinctio quae praesentium\nbeatae alias amet delectus qui voluptate distinctio\nodit sint accusantium autem omnis\nquo molestiae omnis ea eveniet optio"
   },
   {
     "userId": 7,
@@ -801,24 +510,6 @@ var posts = [
   },
   {
     "userId": 9,
-    "id": 82,
-    "title": "laudantium voluptate suscipit sunt enim enim",
-    "body": "ut libero sit aut totam inventore sunt\nporro sint qui sunt molestiae\nconsequatur cupiditate qui iste ducimus adipisci\ndolor enim assumenda soluta laboriosam amet iste delectus hic"
-  },
-  {
-    "userId": 9,
-    "id": 83,
-    "title": "odit et voluptates doloribus alias odio et",
-    "body": "est molestiae facilis quis tempora numquam nihil qui\nvoluptate sapiente consequatur est qui\nnecessitatibus autem aut ipsa aperiam modi dolore numquam\nreprehenderit eius rem quibusdam"
-  },
-  {
-    "userId": 9,
-    "id": 84,
-    "title": "optio ipsam molestias necessitatibus occaecati facilis veritatis dolores aut",
-    "body": "sint molestiae magni a et quos\neaque et quasi\nut rerum debitis similique veniam\nrecusandae dignissimos dolor incidunt consequatur odio"
-  },
-  {
-    "userId": 9,
     "id": 85,
     "title": "dolore veritatis porro provident adipisci blanditiis et sunt",
     "body": "similique sed nisi voluptas iusto omnis\nmollitia et quo\nassumenda suscipit officia magnam sint sed tempora\nenim provident pariatur praesentium atque animi amet ratione"
@@ -861,36 +552,6 @@ var posts = [
   },
   {
     "userId": 10,
-    "id": 92,
-    "title": "ratione ex tenetur perferendis",
-    "body": "aut et excepturi dicta laudantium sint rerum nihil\nlaudantium et at\na neque minima officia et similique libero et\ncommodi voluptate qui"
-  },
-  {
-    "userId": 10,
-    "id": 93,
-    "title": "beatae soluta recusandae",
-    "body": "dolorem quibusdam ducimus consequuntur dicta aut quo laboriosam\nvoluptatem quis enim recusandae ut sed sunt\nnostrum est odit totam\nsit error sed sunt eveniet provident qui nulla"
-  },
-  {
-    "userId": 10,
-    "id": 94,
-    "title": "qui qui voluptates illo iste minima",
-    "body": "aspernatur expedita soluta quo ab ut similique\nexpedita dolores amet\nsed temporibus distinctio magnam saepe deleniti\nomnis facilis nam ipsum natus sint similique omnis"
-  },
-  {
-    "userId": 10,
-    "id": 95,
-    "title": "id minus libero illum nam ad officiis",
-    "body": "earum voluptatem facere provident blanditiis velit laboriosam\npariatur accusamus odio saepe\ncumque dolor qui a dicta ab doloribus consequatur omnis\ncorporis cupiditate eaque assumenda ad nesciunt"
-  },
-  {
-    "userId": 10,
-    "id": 96,
-    "title": "quaerat velit veniam amet cupiditate aut numquam ut sequi",
-    "body": "in non odio excepturi sint eum\nlabore voluptates vitae quia qui et\ninventore itaque rerum\nveniam non exercitationem delectus aut"
-  },
-  {
-    "userId": 10,
     "id": 97,
     "title": "quas fugiat ut perspiciatis vero provident",
     "body": "eum non blanditiis soluta porro quibusdam voluptas\nvel voluptatem qui placeat dolores qui velit aut\nvel inventore aut cumque culpa explicabo aliquid at\nperspiciatis est et voluptatem dignissimos dolor itaque sit nam"
@@ -907,4 +568,7 @@ var posts = [
     "title": "temporibus sit alias delectus eligendi possimus magni",
     "body": "quo deleniti praesentium dicta non quod\naut est molestias\nmolestias et officia quis nihil\nitaque dolorem quia"
   },
-]
+];
+
+var users2 =[];
+var posts2 =[];
